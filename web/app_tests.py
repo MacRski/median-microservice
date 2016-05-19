4a0f58ade683f3d360ec7ccc2e668399c293ad4f
Jonathan Ellenberger <jellenberger@massmutual.com>
2016-05-19 15:33:13 -0400

    Adding comment above redis url hi-jack

--- web/app_tests.py
+++ web/app_tests.py
@@ -6,7 +6,9 @@ import unittest
 from flask import json
 from rq import Connection, Queue, SimpleWorker
 
+# Setting redis URL for testing
 os.environ['REDISTOGO_URL'] = 'redis://localhost:6379'
+
 from app import app as microservice
 from worker import redis_conn
 
